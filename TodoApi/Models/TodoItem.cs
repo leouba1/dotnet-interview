fc5b945ff4ad02af12b9135b4968edb46323be3f
Leo Uba <uba.leandro@gmail.com>
2025-08-25 20:28:33 +0000

    Adding TodoItem migrations

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -4,6 +4,6 @@ public class TodoItem
 {
     public long Id { get; set; }
     public long TodoListId { get; set; }
-    public required string Description { get; set; }
+    public string Description { get; set; } = null!;
     public bool IsCompleted { get; set; }
 }
