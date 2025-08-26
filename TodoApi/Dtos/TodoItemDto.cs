17b8104bc6dd76bffec3a6ce76770511855ff0ed
Leo Uba <uba.leandro@gmail.com>
2025-08-25 21:29:30 -0300

    Add Id to TodoItemDto and update controller

--- TodoApi/Dtos/TodoItemDto.cs
+++ TodoApi/Dtos/TodoItemDto.cs
@@ -2,6 +2,7 @@ namespace TodoApi.Dtos;
 
 public class TodoItemDto
 {
+    public long Id { get; set; }
     public required string Description { get; set; }
     public bool IsCompleted { get; set; }
 }
